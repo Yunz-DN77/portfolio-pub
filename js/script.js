1f72c6aca88e3d40e06581a1c262648ece3db7e8
Yunz-DN77 <77indestructible@gmail.com>
2023-07-29 21:25:23 +0700

    menambah file

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const burger = document.querySelector('header div.burger')
+const nav = document.querySelector('header nav')
+
+burger.addEventListener('click', function () {
+    nav.classList.toggle('active')
+    burger.classList.toggle('active')
+})
+
+document.addEventListener('click', function (e){
+    if (!burger.contains(e.target) && !nav.contains(e.target)){
+        nav.classList.remove('active')
+        burger.classList.remove('active')
+    }
+}
+)
